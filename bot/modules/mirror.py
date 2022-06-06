77f3a4ffcda4306d7a22b2269c78e100b95d0d5a
bowchaw <105728993+bowchaw@users.noreply.github.com>
2022-06-06 14:56:24 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -64,7 +64,7 @@ class MirrorListener:
         if not self.isPrivate and INCOMPLETE_TASK_NOTIFIER and DB_URI is not None:
             DbManger().add_incomplete_task(self.message.chat.id, self.message.link, self.tag)
 
-    def onDownloadComplete(self):
+    def onDownloadComplete(self, link: str, size, files, folders, typ, name: str):
         with download_dict_lock:
             LOGGER.info(f"Download completed: {download_dict[self.uid].name()}")
             download = download_dict[self.uid]
@@ -74,104 +74,10 @@ class MirrorListener:
             if name == "None" or self.isQbit or not ospath.exists(f'{DOWNLOAD_DIR}{self.uid}/{name}'):
                 name = listdir(f'{DOWNLOAD_DIR}{self.uid}')[-1]
             m_path = f'{DOWNLOAD_DIR}{self.uid}/{name}'
-        if self.isZip:
-            try:
-                with download_dict_lock:
-                    download_dict[self.uid] = ZipStatus(name, m_path, size)
-                path = m_path + ".zip"
-                LOGGER.info(f'Zip: orig_path: {m_path}, zip_path: {path}')
-                if self.pswd is not None:
-                    if self.isLeech and int(size) > TG_SPLIT_SIZE:
-                        srun(["7z", f"-v{TG_SPLIT_SIZE}b", "a", "-mx=0", f"-p{self.pswd}", path, m_path])
-                    else:
-                        srun(["7z", "a", "-mx=0", f"-p{self.pswd}", path, m_path])
-                elif self.isLeech and int(size) > TG_SPLIT_SIZE:
-                    srun(["7z", f"-v{TG_SPLIT_SIZE}b", "a", "-mx=0", path, m_path])
-                else:
-                    srun(["7z", "a", "-mx=0", path, m_path])
-            except FileNotFoundError:
-                LOGGER.info('File to archive not found!')
-                self.onUploadError('Internal error occurred!!')
-                return
-            if not self.isQbit or not QB_SEED or self.isLeech:
-                try:
-                    rmtree(m_path)
-                except:
-                    osremove(m_path)
-        elif self.extract:
-            try:
-                if ospath.isfile(m_path):
-                    path = get_base_name(m_path)
-                LOGGER.info(f"Extracting: {name}")
-                with download_dict_lock:
-                    download_dict[self.uid] = ExtractStatus(name, m_path, size)
-                if ospath.isdir(m_path):
-                    for dirpath, subdir, files in walk(m_path, topdown=False):
-                        for file_ in files:
-                            if file_.endswith(".zip") or re_search(r'\.part0*1\.rar$|\.7z\.0*1$|\.zip\.0*1$', file_) \
-                               or (file_.endswith(".rar") and not re_search(r'\.part\d+\.rar$', file_)):
-                                m_path = ospath.join(dirpath, file_)
-                                if self.pswd is not None:
-                                    result = srun(["7z", "x", f"-p{self.pswd}", m_path, f"-o{dirpath}", "-aot"])
-                                else:
-                                    result = srun(["7z", "x", m_path, f"-o{dirpath}", "-aot"])
-                                if result.returncode != 0:
-                                    LOGGER.error('Unable to extract archive!')
-                        for file_ in files:
-                            if file_.endswith((".rar", ".zip")) or re_search(r'\.r\d+$|\.7z\.\d+$|\.z\d+$|\.zip\.\d+$', file_):
-                                del_path = ospath.join(dirpath, file_)
-                                osremove(del_path)
-                    path = f'{DOWNLOAD_DIR}{self.uid}/{name}'
-                else:
-                    if self.pswd is not None:
-                        result = srun(["bash", "pextract", m_path, self.pswd])
-                    else:
-                        result = srun(["bash", "extract", m_path])
-                    if result.returncode == 0:
-                        LOGGER.info(f"Extracted Path: {path}")
-                        osremove(m_path)
-                    else:
-                        LOGGER.error('Unable to extract archive! Uploading anyway')
-                        path = f'{DOWNLOAD_DIR}{self.uid}/{name}'
-            except NotSupportedExtractionArchive:
-                LOGGER.info("Not any valid archive, uploading file as it is.")
-                path = f'{DOWNLOAD_DIR}{self.uid}/{name}'
-        else:
-            path = f'{DOWNLOAD_DIR}{self.uid}/{name}'
-        up_name = PurePath(path).name
-        up_path = f'{DOWNLOAD_DIR}{self.uid}/{up_name}'
-        if self.isLeech and not self.isZip:
-            checked = False
-            for dirpath, subdir, files in walk(f'{DOWNLOAD_DIR}{self.uid}', topdown=False):
-                for file_ in files:
-                    f_path = ospath.join(dirpath, file_)
-                    f_size = ospath.getsize(f_path)
-                    if int(f_size) > TG_SPLIT_SIZE:
-                        if not checked:
-                            checked = True
-                            with download_dict_lock:
-                                download_dict[self.uid] = SplitStatus(up_name, up_path, size)
-                            LOGGER.info(f"Splitting: {up_name}")
-                        split_file(f_path, f_size, file_, dirpath, TG_SPLIT_SIZE)
-                        osremove(f_path)
-        if self.isLeech:
-            size = get_path_size(f'{DOWNLOAD_DIR}{self.uid}')
-            LOGGER.info(f"Leech Name: {up_name}")
-            tg = TgUploader(up_name, self)
-            tg_upload_status = TgUploadStatus(tg, size, gid, self)
-            with download_dict_lock:
-                download_dict[self.uid] = tg_upload_status
-            update_all_messages()
-            tg.upload()
-        else:
-            size = get_path_size(up_path)
-            LOGGER.info(f"Upload Name: {up_name}")
-            drive = GoogleDriveHelper(up_name, self)
-            upload_status = UploadStatus(drive, size, gid, self)
-            with download_dict_lock:
-                download_dict[self.uid] = upload_status
-            update_all_messages()
-            drive.upload(up_name)
+        if not self.isPrivate and INCOMPLETE_TASK_NOTIFIER and DB_URI is not None:
+            DbManger().rm_complete_task(self.message.link)
+        msg = f"<b>Name: </b><code>{escape(name)}</code>\n\n<b>Size: </b>{size}"
+
 
     def onDownloadError(self, error):
         error = error.replace('<', ' ').replace('>', ' ')
@@ -184,95 +90,6 @@ class MirrorListener:
             count = len(download_dict)
         msg = f"{self.tag} your download has been stopped due to: {error}"
         sendMessage(msg, self.bot, self.message)
-        if count == 0:
-            self.clean()
-        else:
-            update_all_messages()
-
-        if not self.isPrivate and INCOMPLETE_TASK_NOTIFIER and DB_URI is not None:
-            DbManger().rm_complete_task(self.message.link)
-
-    def onUploadComplete(self, link: str, size, files, folders, typ, name: str):
-        if not self.isPrivate and INCOMPLETE_TASK_NOTIFIER and DB_URI is not None:
-            DbManger().rm_complete_task(self.message.link)
-        msg = f"<b>Name: </b><code>{escape(name)}</code>\n\n<b>Size: </b>{size}"
-        if self.isLeech:
-            msg += f'\n<b>Total Files: </b>{folders}'
-            if typ != 0:
-                msg += f'\n<b>Corrupted Files: </b>{typ}'
-            msg += f'\n<b>cc: </b>{self.tag}\n\n'
-            if not files:
-                sendMessage(msg, self.bot, self.message)
-            else:
-                fmsg = ''
-                for index, (link, name) in enumerate(files.items(), start=1):
-                    fmsg += f"{index}. <a href='{link}'>{name}</a>\n"
-                    if len(fmsg.encode() + msg.encode()) > 4000:
-                        sendMessage(msg + fmsg, self.bot, self.message)
-                        sleep(1)
-                        fmsg = ''
-                if fmsg != '':
-                    sendMessage(msg + fmsg, self.bot, self.message)
-        else:
-            msg += f'\n\n<b>Type: </b>{typ}'
-            if ospath.isdir(f'{DOWNLOAD_DIR}{self.uid}/{name}'):
-                msg += f'\n<b>SubFolders: </b>{folders}'
-                msg += f'\n<b>Files: </b>{files}'
-            msg += f'\n\n<b>cc: </b>{self.tag}'
-            buttons = ButtonMaker()
-            link = short_url(link)
-            buttons.buildbutton("☁️ Drive Link", link)
-            LOGGER.info(f'Done Uploading {name}')
-            if INDEX_URL is not None:
-                url_path = rutils.quote(f'{name}')
-                share_url = f'{INDEX_URL}/{url_path}'
-                if ospath.isdir(f'{DOWNLOAD_DIR}/{self.uid}/{name}'):
-                    share_url += '/'
-                    share_url = short_url(share_url)
-                    buttons.buildbutton("⚡ Index Link", share_url)
-                else:
-                    share_url = short_url(share_url)
-                    buttons.buildbutton("⚡ Index Link", share_url)
-                    if VIEW_LINK:
-                        share_urls = f'{INDEX_URL}/{url_path}?a=view'
-                        share_urls = short_url(share_urls)
-                        buttons.buildbutton("🌐 View Link", share_urls)
-            if BUTTON_FOUR_NAME is not None and BUTTON_FOUR_URL is not None:
-                buttons.buildbutton(f"{BUTTON_FOUR_NAME}", f"{BUTTON_FOUR_URL}")
-            if BUTTON_FIVE_NAME is not None and BUTTON_FIVE_URL is not None:
-                buttons.buildbutton(f"{BUTTON_FIVE_NAME}", f"{BUTTON_FIVE_URL}")
-            if BUTTON_SIX_NAME is not None and BUTTON_SIX_URL is not None:
-                buttons.buildbutton(f"{BUTTON_SIX_NAME}", f"{BUTTON_SIX_URL}")
-            sendMarkup(msg, self.bot, self.message, InlineKeyboardMarkup(buttons.build_menu(2)))
-            if self.isQbit and QB_SEED and not self.extract:
-                if self.isZip:
-                    try:
-                        osremove(f'{DOWNLOAD_DIR}{self.uid}/{name}')
-                    except:
-                        pass
-                return
-        clean_download(f'{DOWNLOAD_DIR}{self.uid}')
-        with download_dict_lock:
-            try:
-                del download_dict[self.uid]
-            except Exception as e:
-                LOGGER.error(str(e))
-            count = len(download_dict)
-        if count == 0:
-            self.clean()
-        else:
-            update_all_messages()
-
-    def onUploadError(self, error):
-        e_str = error.replace('<', '').replace('>', '')
-        clean_download(f'{DOWNLOAD_DIR}{self.uid}')
-        with download_dict_lock:
-            try:
-                del download_dict[self.uid]
-            except Exception as e:
-                LOGGER.error(str(e))
-            count = len(download_dict)
-        sendMessage(f"{self.tag} {e_str}", self.bot, self.message)
         if count == 0:
             self.clean()
         else:
